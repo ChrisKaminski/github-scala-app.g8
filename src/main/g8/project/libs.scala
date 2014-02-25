b8dcee515789c670d56beadc65c1de410727332f
Chris Kaminski <chris.t.kaminski@gmail.com>
2014-02-25 03:03:36 -0800

    Add slf4j adapter for logback2

--- src/main/g8/project/libs.scala
+++ src/main/g8/project/libs.scala
@@ -2,9 +2,12 @@ import sbt._
 import Keys._
 
 object Libs {
+  lazy val log4j2Version = "2.0-rc1"
+
   lazy val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.2.3"
-  lazy val log4j2Api = "org.apache.logging.log4j" % "log4j-api" % "2.0-rc1"
-  lazy val log4j2Core = "org.apache.logging.log4j" % "log4j-core" % "2.0-rc1"
+  lazy val log4j2Api = "org.apache.logging.log4j" % "log4j-api" % log4j2Version
+  lazy val log4j2Core = "org.apache.logging.log4j" % "log4j-core" % log4j2Version
+  lazy val log4jSlf4jImpl = "org.apache.logging.log4j" % "log4j-slf4j-impl" % log4j2Version
   lazy val metricsScala = "nl.grons" %% "metrics-scala" % "3.0.4"
   lazy val scalalogging = "com.typesafe" %% "scalalogging-log4j" % "1.1.0"
   lazy val scalatest = "org.scalatest" %% "scalatest" % "2.0" % "test"
@@ -12,6 +15,7 @@ object Libs {
 
   lazy val common = Seq(
     log4j2Api,
+    log4jSlf4jImpl,
     akkaActor,
     metricsScala,
     scalalogging,
